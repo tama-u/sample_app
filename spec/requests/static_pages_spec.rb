188dd9ec1a63ad76c4c8571bab5cc94e236eb072
tama <tama.u0314@gmail.com>
2014-06-12 23:04:08 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,23 +8,36 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
   
-  describe "Help page" do
+   describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-  end
-
-  describe "About page" do
-
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
+  
+   describe "About page" do
 
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+  
 end
-
